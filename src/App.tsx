325bcf9244fed120e0e66a6ac453041cd0930b35
Daniel Garcia <pel_daniel@hotmail.com>
2023-09-22 18:04:22 -0700

    feat: init tsx tree-sitter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
 import './App.css'
+import Parser from 'web-tree-sitter'
 
 export function App() {
+  useEffect(() => {
+    const initTreeSitter = async () => {
+      await Parser.init()
+      const parser = new Parser()
+
+      const Tsx = await Parser.Language.load('/tree-sitter-languages/tree-sitter-tsx.wasm');
+      parser.setLanguage(Tsx)
+
+      const sourceCode = 'let x = 1; console.log(x);';
+      const tree = parser.parse(sourceCode);
+    }
+
+    initTreeSitter()
+  }, [])
+
   return (
     <>
       <h1>Vite + React</h1>
