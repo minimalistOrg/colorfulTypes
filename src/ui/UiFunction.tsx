c1211a54d6d01683a7dae2494867d3138c51d20b
Daniel Garcia <pel_daniel@hotmail.com>
2023-10-06 21:22:54 -0700

    feat: add UiFunction component

---
+++ src/ui/UiFunction.tsx
@@ -0,0 +1,23 @@
+import classNames from "classnames";
+
+import { MyFunction } from "../utils/parser";
+import { nameToColor } from "../utils/nameToColor";
+import { UiType } from "./UiType";
+
+import styles from './UiFunction.module.css';
+
+export const UiFunction = ({
+  myFunction,
+}: {
+  myFunction: MyFunction;
+}) => {
+  return (
+    <div className={classNames('flex', styles.uiFunction)}>
+      <p>(</p>
+      {myFunction.arguments.map(myArgument => (
+        <UiType color={nameToColor(myArgument.type)} />
+      ))}
+      <p>)</p>
+    </div>
+  )
+};
