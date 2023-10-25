3d03b74529cfdd49bff03aebc5ad2812b40d3744
Daniel Garcia <pel_daniel@hotmail.com>
2023-10-24 20:36:00 -0700

    test: add test for arrow function with no params

--- src/utils/tests/parser.test.ts
+++ src/utils/tests/parser.test.ts
@@ -1,5 +1,5 @@
-import { expect, test } from 'vitest'
-import { buildInterface } from '../parser'
+import { expect, test, describe, it } from 'vitest'
+import { buildFunction, buildInterface } from '../parser'
 import { emptySyntaxNode, mockQueryCapture, mockSyntaxNode } from './mocks'
 
 test('build interface', () => {
@@ -8,8 +8,8 @@ test('build interface', () => {
     children: [
       emptySyntaxNode(),
       mockSyntaxNode({
-        children: [mockSyntaxNode({ children: []})],
-        text: 'MyInterface'
+        text: 'MyInterface',
+        children: [],
       })
     ]
   });
@@ -20,5 +20,51 @@ test('build interface', () => {
       start: { row: 0, column: 0 },
       end: { row: 0, column: 0 }
     }
+  });
+});
+
+describe('build function', () => {
+  it('builds an arrow function with no params', () => {
+    const functionCapture = mockQueryCapture({
+      name: 'definition.function',
+      children: [
+        mockSyntaxNode({ text: 'const' }),
+        mockSyntaxNode({
+          text: 'arrowFunctionName = (): void => {}',
+          children: [
+            mockSyntaxNode({ text: 'arrowFunctionName'}),
+            mockSyntaxNode({ text: '='}),
+            mockSyntaxNode({
+              text: '(): void => {}',
+              children: [
+                mockSyntaxNode({
+                  text: '()',
+                  children: [
+                    mockSyntaxNode({ text: '(' }),
+                    mockSyntaxNode({ text: ')' }),
+                  ],
+                }),
+                mockSyntaxNode({ text: ': void' }),
+                mockSyntaxNode({ text: '=>' }),
+                mockSyntaxNode({ text: '{}' }),
+              ]
+            }),
+          ],
+        })
+      ]
+    });
+
+    expect(buildFunction(functionCapture)).toStrictEqual({
+      name: 'arrowFunctionName',
+      parameters: [],
+      returnType: {
+        type: 'void',
+        predefinedType: true,
+      },
+      codebasePosition: {
+        start: { row: 0, column: 0 },
+        end: { row: 0, column: 0 }
+      }
+    });
   });
 });
