12bc366b506715a5fb30c2fa9a3650cd15a2902a
Daniel Garcia <pel_daniel@hotmail.com>
2023-11-22 19:43:24 -0800

    feat: show function name in zoomLevel 2

--- src/ui/UiFunction.tsx
+++ src/ui/UiFunction.tsx
@@ -15,17 +15,38 @@ export const UiFunction = ({
   zoomLevel?: ZoomLevel
 }) => {
   const columns = zoomLevel === 1 ? Math.ceil(Math.sqrt(myFunction.parameters.length)) : 1;
+  const zeroParameters = myFunction.parameters.length === 0;
 
   return (
     <div
       className={classnames(
         styles.uiFunction,
         styles[`uiFunctionLevel${zoomLevel}`],
-        { [styles['uiFunctionSameLineLevel2']]: myFunction.parameters.length === 0 }
+        { [styles['uiFunctionSameLineLevel2']]: zeroParameters }
       )}
     >
+      {
+        zoomLevel === 2 && (
+          <p
+            className={
+              classnames(
+                styles.uiFunctionName,
+                { [styles.uiFunctionNameMargin]: !zeroParameters }
+              )
+            }
+          >
+            {myFunction.name}
+          </p>
+        )
+      }
+
       <div
-        className={classnames(styles.uiParameterGroup, styles[`uiParameterGroupLevel${zoomLevel}`])}
+        className={
+          classnames(
+            styles.uiParameterGroup,
+            { [styles[`uiParameterGroupLevel${zoomLevel}`]]: !zeroParameters }
+          )
+        }
       >
         <p className={styles.uiSymbol}>(</p>
 
@@ -35,7 +56,11 @@ export const UiFunction = ({
         >
           {myFunction.parameters.map(myParameter => (
             <div className="flex flex-vcenter">
-              {zoomLevel === 2 && <p>{`${myParameter.name}:`}</p>}
+              {
+                zoomLevel === 2 && (
+                  <p className={styles.uiParameterLevel2}>{`${myParameter.name}:`}</p>
+                )
+              }
 
               <UiType
                 name={myParameter.type}
