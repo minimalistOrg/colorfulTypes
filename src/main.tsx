3d9e7a56332f4324c328aa92ad64b443d7849e5a
Daniel Garcia <pel_daniel@hotmail.com>
2025-07-07 20:02:49 -0700

    feat: add CodeMap component and route

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import { ColorfulTypes } from './ColorfulTypes.tsx'
+import { CodeMap } from './ui/CodeMap.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ColorfulTypes />} />
+        <Route path="/codemap/:org/:repo" element={<CodeMap />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ src/ui/CodeMap.tsx
@@ -0,0 +1,7 @@
+export const CodeMap = () => {
+  return (
+    <>
+      <h1>Code map</h1>
+    </>
+  )
+}
